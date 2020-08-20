b6790da4a0a35bc04f7a2a0c98d06466307f2510
Gavin <tianyaogavin@gmail.com>
2020-08-20 16:23:15 +0800

    添加区块链模块文字描述

---
+++ src/pages/chainMng/explorer/locales/zh-CN.ts
@@ -0,0 +1,6 @@
+export default {
+  "chainMng.explorer.blockheight":"区块高度",
+  "chainMng.explorer.txamount":"交易数量",
+  "chainMng.explorer.realtimetx":"实时交易",
+  "chainMng.explorer.latestblocks":"最新区块"
+};
